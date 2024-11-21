bdaa97d0006afe2a44c1963fe999ee88e8e17787
Kvezac <Kvezak@gmail.com>
2024-11-22 02:34:41 +0300

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.  Changes to be committed: 	modified:   main.py

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,28 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
+
 
 @app.get("/tasks")
 def get_tasks():
     pass
 
+
 @app.post("/tasks")
 def create_task(task):
     pass
+
 
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int):
     pass
 
+
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
     pass
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=app, host='localhost', port=8000)
