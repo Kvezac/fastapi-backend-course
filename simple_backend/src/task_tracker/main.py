0f6b9b558a0faeeda28f38e778d4f1157adca4ac
Kvezac <Kvezak@gmail.com>
2024-11-22 04:51:56 +0300

    state management at list  On branch second_simple_background_task  Changes to be committed:
    	new file:   README.md
    	modified:   main.py
    	modified:   pyproject.toml
    
    	new file:   README.md
    	modified:   main.py
    	modified:   pyproject.toml

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,28 +1,72 @@
+from typing import List
+
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
-@app.get("/tasks")
+class Task(BaseModel):
+    id: int
+    title: str
+    status: bool
+
+
+tasks: List[Task] = []
+
+
+def get_task_id():
+    logging.info('get task id')
+    if not tasks:
+        return 1
+    id = max(tasks, key=lambda task: task['id'])
+    logging.info('max task id: %s', id['id'])
+    id = id['id'] + 1
+    logging.info('next task id: %s', id)
+    return id
+
+
+@app.get("/tasks", response_model=List[Task])
 def get_tasks():
-    pass
+    logging.info('all task status True')
+    task_all = [task for task in tasks if task['status']]
+    return task_all
 
 
-@app.post("/tasks")
-def create_task(task):
-    pass
+@app.post("/tasks", response_model=Task)
+def create_task(title):
+    logging.info('create task')
+    id = get_task_id()
+    task = {'id': id,
+            'title': title,
+            'status': True}
+    tasks.append(task)
+    logging.info('task id: %s title: %s done', id, title)
+    return task
 
 
-@app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+@app.put("/tasks/{task_id}", response_model=Task)
+def update_task(task_id: int, title: str):
+    logging.info(msg=f'change tittle task {task_id = }')
+    task = [task for task in tasks if task['id'] == task_id][0]
+    task['title'] = title
+    logging.info(msg=f'good title task {task_id = } is change {title = }')
+    return task
 
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    logging.info('delete task id: %s', task_id)
+    task = [task for task in tasks if task['id'] == task_id][0]
+    task['status'] = False
+    logging.info('task id: %s deleted', task_id)
+    return {"detail": "Task deleted"}
 
 
 if __name__ == '__main__':
-    uvicorn.run(app=app, host='localhost', port=8000)
+    uvicorn.run(app='main:app', host='localhost', port=8000, reload=True)
