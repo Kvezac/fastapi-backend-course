ca25007e7294bc046b78bc32e5c09efb54066648
Kvezac <Kvezak@gmail.com>
2024-11-23 03:30:10 +0300

    storage in json file
     On branch extended_simple_background_task
     Changes to be committed:
    	modified:   README.md
    	new file:   config.py
    	new file:   data_task.json
    	new file:   logger/__init__.py
    	new file:   logger/common.py
    	modified:   main.py

---
+++ simple_backend/src/task_tracker/config.py
@@ -0,0 +1,5 @@
+class Settings:
+    file_name: str = 'data_task.json'
+
+
+settings = Settings()
--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,12 +1,18 @@
+import json
+import logging
+
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-import logging
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+from config import settings
+from logger.common import configure_logging
+
+configure_logging()
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
+storage: str = settings.file_name
 
 
 class Task(BaseModel):
@@ -15,55 +21,91 @@ class Task(BaseModel):
     status: bool
 
 
-tasks: list[Task] = []
+def load_tasks(folder: str) -> list:
+    logging.info('open tasks folder for read')
+    try:
+        with open(folder, 'r', encoding='utf-8') as file_r:
+            data = json.load(file_r)
+    except (IOError, json.JSONDecodeError) as e:
+        logging.warning('Exception %s', e)
+        return []
+    else:
+        logging.info('return data')
+        return data
+
+
+def save_tasks(folder: str, tasks: list) -> None:
+    logging.info('open tasks folder for write')
+    try:
+        with open(folder, 'w', encoding='utf-8') as file_w:
+            json.dump(tasks, file_w, indent=4)
+    except IOError as e:
+        logging.error('Exception %s', e)
 
 
 def get_task_id():
     logging.info('get task id')
+    tasks = load_tasks(storage)
+    logging.info(tasks)
     if not tasks:
         return 1
-    id = max(tasks, key=lambda task: task['id'])
-    logging.info('max task id: %s', id['id'])
-    id = id['id'] + 1
-    logging.info('next task id: %s', id)
-    return id
+    max_id = max(task['id'] for task in tasks)
+    logging.info('max task id: %s', max_id)
+    next_id = max_id + 1
+    logging.info('next task id: %s', next_id)
+    return next_id
 
 
 @app.get("/tasks", response_model=list[Task])
 def get_tasks():
     logging.info('all task status True')
+    tasks = load_tasks(storage)
     task_all = [task for task in tasks if task['status']]
     return task_all
 
 
 @app.post("/tasks", response_model=Task)
-def create_task(title):
+def create_task(title: str):
     logging.info('create task')
     id = get_task_id()
     task = {'id': id,
             'title': title,
             'status': True}
+    tasks = load_tasks(storage)
     tasks.append(task)
+    save_tasks(storage, tasks)
     logging.info('task id: %s title: %s done', id, title)
     return task
 
 
 @app.put("/tasks/{task_id}", response_model=Task)
 def update_task(task_id: int, title: str):
-    logging.info(msg=f'change tittle task {task_id = }')
-    task = [task for task in tasks if task['id'] == task_id][0]
-    task['title'] = title
-    logging.info(msg=f'good title task {task_id = } is change {title = }')
-    return task
+    logging.info(msg=f'change title task {task_id = }')
+    tasks = load_tasks(storage)
+    task = next((task for task in tasks if task['id'] == task_id), None)
+    if task:
+        task['title'] = title
+        save_tasks(storage, tasks)
+        logging.info(msg=f'good title task {task_id = } is change {title = }')
+        return task
+    else:
+        logging.warning(f'Task with id {task_id} not found')
+        return {"detail": "Task not found"}
 
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
     logging.info('delete task id: %s', task_id)
-    task = [task for task in tasks if task['id'] == task_id][0]
-    task['status'] = False
-    logging.info('task id: %s deleted', task_id)
-    return {"detail": "Task deleted"}
+    tasks = load_tasks(storage)
+    task = next((task for task in tasks if task['id'] == task_id), None)
+    if task:
+        task['status'] = False
+        save_tasks(storage, tasks)
+        logging.info('task id: %s deleted', task_id)
+        return {"detail": "Task deleted"}
+    else:
+        logging.warning(f'Task with id {task_id} not found')
+        return {"detail": "Task not found"}
 
 
 if __name__ == '__main__':
